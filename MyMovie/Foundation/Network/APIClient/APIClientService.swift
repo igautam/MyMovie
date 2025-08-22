76124b3a5b13bf6a58662bac69f3329e2b1960c3
igautam <igautamraj15@gmail.com>
2025-08-22 13:34:02 +0530

    handled Swift lint warning for APIClientService

--- MyMovie/Foundation/Network/APIClient/APIClientService.swift
+++ MyMovie/Foundation/Network/APIClient/APIClientService.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 final class APIClientService: APIClientServiceInterface {
-    func request<T>(_ urlString: String, for type: T.Type) async throws -> T where T : Decodable {
+    func request<T>(_ urlString: String, for type: T.Type) async throws -> T where T: Decodable {
         guard let url = URL(string: urlString) else {
             throw APIError.invalidURL
         }
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
-        guard let jsonResponse = try? JSONDecoder().decode(T.self, from: data), (response as? HTTPURLResponse)?.statusCode == HttpStatusCode.success.rawValue else {
+        guard let jsonResponse = try? JSONDecoder()
+            .decode(T.self, from: data),
+              (response as? HTTPURLResponse)?.statusCode == HttpStatusCode.success.rawValue else {
             throw APIError.invalidData
         }
         return jsonResponse
